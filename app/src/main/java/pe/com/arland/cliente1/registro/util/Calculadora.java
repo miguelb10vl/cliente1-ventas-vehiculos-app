a190d24c38e0031f925d9d60a8d438071d1e8e1f
Miguel Blas <miguel.blas@vasslatam.com>
2020-11-19 19:24:28 -0500

    session3

--- app/src/main/java/pe/com/arland/cliente1/registro/util/Calculadora.java
+++ app/src/main/java/pe/com/arland/cliente1/registro/util/Calculadora.java
@@ -8,4 +8,12 @@ public class Calculadora {
 	public static double resta(double a , double b) {
 		return a - b ;
 	}
+
+	public static double multiplicacion(double a , double b) {
+		return a * b ;
+	}
+	
+	public static double division(double a , double b) {
+		return a / b ;
+	}
 }
